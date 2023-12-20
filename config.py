fecfa006364d17f263acc15f907696b6ffb29b53
qsqisoon <111358582+qsqisoon@users.noreply.github.com>
2023-12-20 08:38:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cbrqxfxpfoge.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "khooqisoon-employee"
+customregion = "us-east-1"
+
